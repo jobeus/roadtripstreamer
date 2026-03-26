a1d35baa44b3c4814c8f9bdab7da093b60205e0f
Scotty Carmichael <jobeus@gmail.com>
2026-03-26 01:28:33 -0600

    Initial Xcode project scaffold generated by user

---
+++ DualCast/DualCastTests/DualCastTests.swift
@@ -0,0 +1,19 @@
+//
+//  DualCastTests.swift
+//  DualCastTests
+//
+//  Created by Scotty on 3/26/26.
+//
+
+import Testing
+@testable import DualCast
+
+struct DualCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
